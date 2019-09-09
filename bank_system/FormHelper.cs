5f8fbe7110244313615bd5b5629df26ba846e827
Christine Vinaviles <christine.vinaviles@gmail.com>
2019-09-09 13:25:55 +1000

    add confirm method helper

--- bank_system/FormHelper.cs
+++ bank_system/FormHelper.cs
@@ -7,9 +7,26 @@ using System.Threading.Tasks;
 namespace bank_system
 {
     /*
-     * Helper lass to help with input validations.
+     * Helper class to help with input validations.
      */
     class FormHelper
     {
+        public bool Confirm(string SuccessMessage)
+        {
+            string confirm = Console.ReadLine();
+
+            if (String.Equals(confirm.ToLower(), 'y'.ToString()))
+            {
+                Console.WriteLine(SuccessMessage);
+                System.Threading.Thread.Sleep(1500);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Please enter 'y' or 'n' only.");
+                System.Threading.Thread.Sleep(500);
+                return false;
+            }
+        }
     }
 }
